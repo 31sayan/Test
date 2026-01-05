c20281901b6888f149500d69419512acbbf34f60
Sayan Roy <rsayan8926@gmail.com>
2026-01-05 16:45:24 +0530

    Update customer.js

---
+++ customer.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Check the console for the message!</h1>
+    
+    <script>
+        // This is a comment in JavaScript
+        console.log("Hello, World!"); 
+    </script>
+
+    <p>To view the output, open your browser's developer tools (F12 or right-click and Inspect) and go to the Console tab.</p>
+
+</body>
+</html>
