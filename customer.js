1616e6ce0ebf5d7682cb8159e691e97a8e500870
root <root@ip-172-31-93-14.ec2.internal>
2026-01-05 11:19:59 +0000

    Second

--- customer.js
+++ customer.js
@@ -8,6 +8,12 @@
         console.log("Hello, World!"); 
     </script>
 
+	<script>
+        // Get the element with the ID "demo" and change its inner HTML
+        document.getElementById("demo").innerHTML = "Hello JavaScript!";
+    </script	
+
+
     <p>To view the output, open your browser's developer tools (F12 or right-click and Inspect) and go to the Console tab.</p>
 
 </body>
